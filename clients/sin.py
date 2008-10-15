efd1d7361794b1d950d7c32bc02aaff749bfb7c2
zovirl <zovirl@zovirl.com>
2008-10-15 05:08:03 +0000

    take chart name as argument

--- clients/sin.py
+++ clients/sin.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import math
 import urllib2
+import sys
+
+chart_name = sys.argv[1]
+
 
 for i in range(100):
-  url = 'http://stats-monkey.appspot.com/add/sin/%d/%.2f' % (i, math.sin(i/10.0) * 100)
+  host = 'http://stats-monkey.appspot.com'
+  url = '%s/add/%s/%d/%.2f' % (host, chart_name, i, math.sin(i/10.0) * 100)
   print url
   urllib2.urlopen(url)
